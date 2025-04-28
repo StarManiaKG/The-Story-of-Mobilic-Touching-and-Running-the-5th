64b14798054743f07bec62e521ec16b786d11c4a
bitten2up <seant9140@gmail.com>
2025-04-27 20:22:56 -0500

    pray 2

--- android/app/jni/src/Android.mk
+++ android/app/jni/src/Android.mk
@@ -27,6 +27,7 @@ ANDROID := 1
 
 include $(MAKE_DIR)/platform.mk
 include $(MAKE_DIR)/util.mk
+include $(SRC_XTRA)/Makefile.d/xtra.mk
 
 # Compile flags
 
@@ -50,7 +51,6 @@ LOCAL_SRC_FILES += $(call List,$(LOCAL_PATH)/$(SRC_MAIN)/netcode/Sourcefile)
 LOCAL_SRC_FILES += $(call List,$(LOCAL_PATH)/$(SRC_HWR)/Sourcefile)
 LOCAL_SRC_FILES += $(call List,$(LOCAL_PATH)/$(SRC_SDL)/Sourcefile)
 
-include $(SRC_XTRA)/Makefile.d/xtra.mk # loads any extra files
 
 LOCAL_SRC_FILES += $(SRC_SDL)/SDL_main/SDL_android_main.c $(SRC_SDL)/mixer_sound.c $(SRC_SDL)/i_threads.c
 LOCAL_SRC_FILES += $(SRC_MAIN)/comptime.c $(SRC_MAIN)/md5.c
